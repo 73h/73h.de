c71cb354b1c3fcb77b9e9fd3c27fb327ece45040
h73 <heikoschmidt82@gmail.com>
2021-12-27 10:27:27 +0100

    delete https redirect

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import secrets
 
-from flask import Flask, request, redirect
+from flask import Flask
 
 from models.constants import HOSTNAME
 from sites.blog.blog import site_blog
@@ -12,12 +12,6 @@ def create_app():
     app.config['JSON_AS_ASCII'] = False
     app.config['CSRF_ENABLED'] = True
     app.secret_key = secrets.token_hex(24)
-
-    @app.before_request
-    def before_request():
-        if not request.is_secure:
-            url = request.url.replace('http://', 'https://', 1)
-            return redirect(url, code=301)
 
     app.register_blueprint(site_home)
     app.register_blueprint(site_blog)
