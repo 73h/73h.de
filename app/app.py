b4cc3396f23ce37a7da54c62a92b758de21ff6d0
h73 <heikoschmidt82@gmail.com>
2021-12-27 11:31:55 +0100

    test https-redirect with app.yaml

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import secrets
 
-from flask import Flask, redirect, request
+from flask import Flask
 
 from models.constants import HOSTNAME
 from sites.blog.blog import site_blog
@@ -13,13 +13,15 @@ def create_app():
     app.config["CSRF_ENABLED"] = True
     app.secret_key = secrets.token_hex(24)
 
+    """
     @app.before_request
     def before_request():
         if not request.is_secure and app.config['ENV'] != "development":
             url = request.url.replace("http://", "https://", 1)
             code = 301
             return redirect(url, code=code)
-
+    """
+    
     app.register_blueprint(site_home)
     app.register_blueprint(site_blog)
 
