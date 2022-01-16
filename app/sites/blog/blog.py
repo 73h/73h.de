3ff0a20a362f83367baeb7ede28ef9443fe54fef
h73 <heikoschmidt82@gmail.com>
2022-01-16 22:56:43 +0100

    add blog login

--- app/sites/blog/blog.py
+++ app/sites/blog/blog.py
@@ -1,11 +1,22 @@
 import os
 
 import pymongo
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request, session
 
-from utils.config import HOSTNAME, DATABASE
+from utils.config import HOSTNAME, DATABASE, PROTOCOL
 
 site_blog = Blueprint("blog", __name__)
+
+
+def get_url(site: str) -> str:
+    url = f"{PROTOCOL}blog.{HOSTNAME}"
+    if site != "index":
+        url = f"{url}/{site}"
+    return url
+
+
+def render(site: str, **kwargs):
+    return render_template(f"blog/{site}.html", url=get_url(site), **kwargs)
 
 
 @site_blog.get("/", host=f"blog.{HOSTNAME}")
@@ -13,4 +24,28 @@ def index():
     client = pymongo.MongoClient(os.environ.get("MONGO_CONNECTION_STRING"), tlsAllowInvalidCertificates=True)
     db = client[DATABASE]
     entries = list(db.entries.find())
-    return render_template("blog/index.html", entries=entries)
+    username = None
+    if "user" in session:
+        username = session['user']
+    print(username)
+    return render(site="index", title="A small tech blog.", entries=entries)
+
+
+def login(**kwargs):
+    return render(site="login", title="Editor login.", **kwargs)
+
+
+@site_blog.get("/login", host=f"blog.{HOSTNAME}")
+def login_get():
+    return login()
+
+
+@site_blog.post("/login", host=f"blog.{HOSTNAME}")
+def login_post():
+    user = request.form.get('user')
+    password = request.form.get('password')
+    if f"{user}:{password}" == os.environ.get("BLOG_CREDENTIALS"):
+        session['user'] = user
+    else:
+        flash('Sorry, the credentials are wrong.')
+    return login()
--- app/utils/config.py
+++ app/utils/config.py
@@ -1,8 +1,10 @@
 import os
 
 if os.environ.get("APP_ENV") == "production":
+    PROTOCOL = "https://"
     HOSTNAME = "73h.de"
     DATABASE = "blog"
 else:
+    PROTOCOL = "http://"
     HOSTNAME = "73h-dev.de"
     DATABASE = "blog-dev"
