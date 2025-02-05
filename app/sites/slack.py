d9fe1fc373b74fce5e5c3e7f68700d5460224feb
73h <73h@gmx.net>
2025-02-05 16:41:47 +0100

    add wie-lange-noch

--- app/sites/slack.py
+++ app/sites/slack.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from flask import Blueprint, request
 
@@ -12,4 +14,15 @@ def index():
     if "token" in request.form and "text" in request.form:
         result = requests.post(POKER_API, data=request.form)
         return result.json(), result.status_code
+    return "", 200
+
+@site_slack.post("/wie-lange-noch", host=host)
+def wie_lange_noch():
+    if "token" in request.form and "text" in request.form:
+        dump = json.dumps(request.form)
+        response = {
+            "response_type": "ephemeral",
+            "text": f"Ich bin noch nicht fertig, aber ich arbeite dran! {dump}"
+        }
+        return response, 200
     return "", 200
