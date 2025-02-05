298a387871b66831b254b80ecf30eb1f50ecbeb1
73h <73h@gmx.net>
2025-02-05 17:53:01 +0100

    wie lange muss er noch

--- app/sites/slack.py
+++ app/sites/slack.py
@@ -22,19 +22,15 @@ def index():
 @site_slack.post("/wie-lange-noch", host=host)
 def wie_lange_noch():
     if "token" in request.form and "user_id" in request.form:
-        if request.form["token"] == os.environ.get("SLACK_TOKEN"):
-            end_date = datetime.datetime(2025, 12, 31, 17, 0, 0)
-            delta = end_date - datetime.datetime.now()
-            anrede = os.environ.get("WIE_LANGE_NOCH_NAME") + " arbeitet"
-            if request.form["user_id"] == os.environ.get("WIE_LANGE_NOCH_ID"):
-                anrede = "Du arbeitest"
-            days = delta.days
-            hours = floor(delta.seconds/60/60)
-            minutes = floor(delta.seconds/60)-hours*60
-            seconds = delta.seconds-(minutes*60+hours*60*60)
-            response = {
-                "response_type": "ephemeral",
-                "text": f"{anrede} noch {days} Tage, {hours} Stunden, {minutes} Minuten und {seconds} Sekunden für uns. :muskel:"
-            }
-            return response, 200
+        end_date = datetime.datetime(2025, 12, 31, 17, 0, 0)
+        delta = end_date - datetime.datetime.now()
+        days = delta.days
+        hours = floor(delta.seconds/60/60)
+        minutes = floor(delta.seconds/60)-hours*60
+        seconds = delta.seconds-(minutes*60+hours*60*60)
+        response = {
+            "response_type": "ephemeral",
+            "text": f"Stephan arbeitet noch {days} Tage, {hours} Stunden, {minutes} Minuten und {seconds} Sekunden für uns. :muskel:"
+        }
+        return response, 200
     return "", 200
