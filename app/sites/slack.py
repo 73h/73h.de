d463eddc818c09265f9bdb01f47947a9b46f49bb
73h <73h@gmx.net>
2025-05-16 23:30:31 +0200

    different variants

--- app/sites/slack.py
+++ app/sites/slack.py
@@ -38,30 +38,47 @@ def rente(name, y, m, d):
     }
     return response, 200
 
-def letters_to_int(length, letters = "") -> str:
-    result = 0
+def letters_to_int(typ, length, letters = "") -> str:
+    result = ""
+    if typ == "linear":
+        result = 0
     letters = letters.upper().replace("Ä", "AE").replace("Ü", "UE").replace("Ö", "OE").replace("ß", "ss")
     letters = re.sub(r"[^A-Z]", "", letters)
     letters = letters.ljust(int(length),"A")[:int(length)]
     i = int(length)-1
     for c in letters:
-        result += (ord(c) - 65) * 26**i
+        if typ == "linear":
+            result += (ord(c) - 65) * 26**i
+        else:
+            result += str(ord(c) - 55)
         i += -1
-    return str(result+1)
+    if typ == "linear":
+        result += 1
+    return str(result)
 
-@site_slack.post("/letters-to-int/<length>", host=host)
-def letters_to_int_route(length):
+@site_slack.post("/letters-to-int/<typ>/<length>", host=host)
+def letters_to_int_route_with_typ(typ, length):
     message = "Gib mir bitte ein paar Buchstaben - ich mag Buchstaben! :smiley:"
     if request and "text" in request.form:
         letters = request.form["text"].split(" ")
         result = []
         for l in letters:
             if l:
-                result.append(letters_to_int(length, l))
+                result.append(letters_to_int(typ, length, l))
         if len(result) > 0:
-            message = " ".join(result)
+            message = " | ".join(result)
     response = {
         "response_type": "ephemeral",
         "text": f"{str(message)}"
     }
     return response, 200
+
+@site_slack.post("/letters-to-int/<length>", host=host)
+def letters_to_int_route(length):
+    return letters_to_int_route_with_typ("blockwise", length)
+
+if __name__ == "__main__":
+    print(letters_to_int("blockwise", 4, "a"))
+    print(letters_to_int("blockwise", 4, "z"))
+    print(letters_to_int("linear", 4, "a"))
+    print(letters_to_int("linear", 4, "z"))
